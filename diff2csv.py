80dad68dba6ad0af2da8f81a1ba211fcd78eb872
Yu Saito <Yu Saito>
2017-05-14 12:11:14 +0900

    Ver1.0 当初予定した全機能を実装

--- diff2csv.py
+++ diff2csv.py
@@ -5,7 +5,7 @@ import os
 import argparse
 
 class diff2csv(object):
-    def diff2csv(self,before_lines,after_lines,csv_filename):
+    def diff2csv(self,before_file_path,after_file_path,csv_filename):
         def break_process(tmp_minus,tmp_plus,before_buff,after_buff):
             before_buff.extend(tmp_minus)
             after_buff.extend(tmp_plus)
@@ -17,11 +17,11 @@ class diff2csv(object):
             elif(count_sub < 0):
                 for x in range(-count_sub):
                     before_buff.append("")
-        def format_output(before_lines,after_lines):
+        def format_output(before_lines,after_lines,csv_filename):
             count_before = 0
             count_after = 0
             output_buff = ""
-            
+
             for (bbuf,abuf) in zip(before_lines,after_lines):
                 if bbuf != "":
                     count_before = count_before+1
@@ -31,15 +31,29 @@ class diff2csv(object):
                     + str(count_after) + "," + abuf.replace("\r","").replace("\n","") + os.linesep
 
             # デバッグ用    
-            print("DEBUG: "+output_buff)
-            with open("test.csv","wb") as f:
-                f.write(output_buff)
+            # print("DEBUG: "+output_buff)
+            # with open("test.csv","wb") as f:
+            #     f.write(output_buff)
+            if csv_filename != "stdout":
+                with open(csv_filename,"wb") as f:
+                    f.write(output_buff)
+            elif csv_filename == "stdout":
+                for val in output_buff:
+                    print(val)
+
+        def read_file(file_path):
+            with open(file_path,"r") as f:
+                data  = f.read()
+            return data.splitlines()
 
         is_minus_flag = 0
         before_buff = []
         after_buff = []
         tmp_minus = []
         tmp_plus = []
+
+        before_lines = read_file(before_file_path)
+        after_lines = read_file(after_file_path)
 
         result_list = self.__get_diff_str(before_lines,after_lines)
         for buf in result_list:
@@ -70,7 +84,7 @@ class diff2csv(object):
                 before_buff.append("")
                 after_buff.append(buf)
         
-        format_output(before_buff,after_buff)
+        format_output(before_buff,after_buff,csv_filename)
             
     def __get_diff_str(self,before_lines,after_lines):
         import difflib
@@ -80,88 +94,17 @@ class diff2csv(object):
         for buf in result:
             if buf[0] != "?":
                 tmp.append(buf)
-        return tmp[1:-1]
+        return tmp[:-1]
 
 
 def main():
-    text1 = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer
-    eu lacus accumsan arcu fermentum euismod. Donec pulvinar porttitor
-    mauris eget magna consequat convallis. Nam sed sem vitae odio
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    adipiscing. Suspendisse eu lectus. In nunc. Duis vulputate tristique
-    enim. Donec quis lectus a justo imperdiet tempus.
-    hoge
-    hoge"""
-    text1_lines = text1.splitlines()
-
-    text2 = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer
-    eu lacus accumsan arcu fermentum euismod. Donec pulvinar, porttitor
-    tellus. Aliquam venenatis. Donec facilisis pharetra tortor. In nec
-    mauris eget magna consequat convallis. Nam cras vitae mi vitae odio
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
-    metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
-    urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
-    suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
-    adipiscing. Duis vulputate tristique enim. Donec quis lectus a justo
-    hoge
-    imperdiet tempus. Suspendisse eu lectus. In nunc. 
-    hoge
-    hoge"""
-    text2_lines = text2.splitlines()
-    text3 = """
-    hoge
-    buff
-    ppp
-    """
-    text3_lines = text3.splitlines()
-    text4 = """
-    hoge
-    hoge
-    hoge
-    ppp
-    """
-    text4_lines = text4.splitlines()
 
     # 引数解析
     parser = argparse.ArgumentParser()
     parser.add_argument("before",help="before file path")
     parser.add_argument("after",help="after file path")
     parser.add_argument("-p","--prefix",help="prefix path to before and after file",default="")
+    parser.add_argument("-o","--output",help="output file path (default:stdout)",default="stdout")
     args = parser.parse_args()
 
     # ファイルパス作成
@@ -176,23 +119,8 @@ def main():
         before_file_path = args.before
         after_file_path = args.after
 
-    # # ファイル読み込み
-    # with open(before_file_path,"r") as f:
-    #     before_lines = f.read()
-    # before_lines = before_lines.splitlines()
-    # with open(after_file_path,"r") as f:
-    #     after_lines = f.read()
-    # after_lines = after_lines.splitlines()
-
-    # ### DEBUG ###
-    # for buf in before_lines:
-    #     print(buf)
-    # for buf in after_lines:
-    #     print(buf)
-    # sys.exit()
-
     d2c = diff2csv()
-    d2c.diff2csv(text1_lines,text2_lines,"hoge")
+    d2c.diff2csv(before_file_path,after_file_path,args.output)
 
 if __name__ == '__main__':
     import sys
